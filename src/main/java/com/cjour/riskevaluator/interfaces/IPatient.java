db95e071af2cd1c45778836c9219bebdf0b05eb9
Clément <jour.clement@gmail.com>
2021-10-04 15:20:55 +0200

    SecondClient declaration

---
+++ src/main/java/com/cjour/riskevaluator/interfaces/IPatient.java
@@ -0,0 +1,19 @@
+package com.cjour.riskevaluator.interfaces;
+
+import com.cjour.riskevaluator.model.Patient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(value="patientClient", url = "localhost:8081")
+public interface IPatient {
+
+        @GetMapping("/")
+        String welcome();
+
+        @GetMapping("/patient/list")
+        ResponseEntity<List<Patient>> getAllPatient();
+}
+
